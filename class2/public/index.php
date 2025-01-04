218c60629f36fd87843d10567235187635ed1fd1
gfloriano11 <gflorianodev@gmail.com>
2025-01-04 13:54:51 -0300

    feat: index pull the basic html

--- class2/public/index.php
+++ class2/public/index.php
@@ -1,8 +1,22 @@
+<?php
+
+    $template = file_get_contents('../src/app/template/structure.html');
+
+    // file_get_contents -> get the structure.html
+    
+    echo $template;
+    
+    // echo to put the html in this page.
+    
+?>
+
 <!DOCTYPE html>
 <html lang="PT-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/social_media.css">
+    <link rel="stylesheet" href="styles/global.css">
     <title>Welcome to The Social Media</title>
 </head>
 <body>

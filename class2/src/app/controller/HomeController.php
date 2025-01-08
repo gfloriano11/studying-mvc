916562b8fd5104acf56c1340a4435fa1abb01103
gfloriano11 <gflorianodev@gmail.com>
2025-01-08 19:48:35 -0300

    feat: HomeController get the structure from home.html

--- class2/src/app/controller/HomeController.php
+++ class2/src/app/controller/HomeController.php
@@ -6,10 +6,22 @@
 
             
             try{
-                                
+
                 $posts = Post::selectPosts();
 
-                echo '<p id=feed_title>Posts Recentes:</p>';
+                $loader = new \Twig\Loader\FilesystemLoader('../src/app/view');
+                $twig = new \Twig\Environment($loader); 
+                $template = $twig->load('home.html');
+                
+                $params = array();
+                $params['posts'] = $posts;
+
+                $content = $template->render($params);
+
+                echo $content;
+                                
+
+                // echo '<p id=feed_title>Posts Recentes:</p>';
                 
 
             } catch (Exception $error){

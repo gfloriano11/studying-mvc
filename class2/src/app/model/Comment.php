89401cd24f9643c29738ba5ed969ab940dcfd462
gfloriano11 <gflorianodev@gmail.com>
2025-01-10 08:34:32 -0300

    feat: function to pull comments

--- class2/src/app/model/Comment.php
+++ class2/src/app/model/Comment.php
@@ -10,18 +10,17 @@
                 throw new Exception("Connection failed: " . $conn->connect_error);
             }
 
-            // var_dump($post_data);
+            var_dump($post_data);
 
             foreach ($post_data as $post) {
                 $id = $post['post_id']; // Acessando o 'id' de cada post
 
-                $query = "SELECT comment_id, user_name, c.content, c.created_at FROM comments AS c
-                INNER JOIN post AS p
-                ON p.post_id = c.post_id
-                WHERE c.post_id = $id
-                ORDER BY p.post_id DESC";
+                $query = "SELECT * FROM comments
+                WHERE post_id = ?";
 
                 $statement = $conn->prepare($query);
+
+                $statement->bind_param('i', $id);
 
                 $statement->execute();
 
@@ -31,6 +30,10 @@
 
                 while($row = $result->fetch_assoc()){
                     $comments_data[] = $row;
+                }
+
+                if(!$comments_data){
+                    throw new Exception("Any Post has Been Posted!");
                 }
 
                 $result->free();

0d222c1d776fca944dcc5fe168ff71df8efb54ad
gfloriano11 <gflorianodev@gmail.com>
2025-01-09 22:55:17 -0300

    fix: restructure this archives to only have their specif content

--- class2/public/index.php
+++ class2/public/index.php
@@ -34,16 +34,3 @@
     
     // echo to put the html in this page.
     
-?>
-
-<!DOCTYPE html>
-<html lang="PT-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/social_media.css">
-    <link rel="stylesheet" href="styles/global.css">
-</head>
-<body>
-</body>
-</html>

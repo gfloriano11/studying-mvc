1ffcca22a65e18d46c4f088a5aadffd9faba7d27
gfloriano11 <gflorianodev@gmail.com>
2025-01-10 08:19:08 -0300

    fix: delete function comments in post model

--- class2/src/app/model/Post.php
+++ class2/src/app/model/Post.php
@@ -39,35 +39,6 @@
             return $data;
         }
 
-        public static function selectPostById($id){
-
-            $conn = Connection::getConn();
-
-            $query = "SELECT * FROM post
-            WHERE post_id = ?";
-
-            $statement = $conn->prepare($query);
-
-            $statement->bind_param('i', $id);
-
-            $statement->execute();
-
-            $result = $statement->get_result();
-
-            $data = [];
-
-            $data = $result->fetch_assoc();
-
-            $result->free();
-
-            if(!$data){
-                throw new Exception("This Post Doesn't Exists.");
-            }
-
-            return $data;
-            
-        }
-
         public static function selectComments($post_data){
 
             $conn = Connection::getConn();

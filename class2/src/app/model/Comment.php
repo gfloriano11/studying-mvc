2bba70f7e88cbe15347d63fb3dfe2a4b475dfb33
gfloriano11 <gflorianodev@gmail.com>
2025-01-11 00:27:17 -0300

    fix: comment model comment because its wrong

--- class2/src/app/model/Comment.php
+++ class2/src/app/model/Comment.php
@@ -1,38 +1,38 @@
 <?php
 
-    class Comment{
+    // class Comment{
         
-        public static function selectComments($post_id){
+    //     public static function selectComments($post_id){
 
-            $conn = Connection::getConn();
+    //         $conn = Connection::getConn();
 
-            if($conn->connect_error){
-                throw new Exception("Connection failed: " . $conn->connect_error);
-            }
+    //         if($conn->connect_error){
+    //             throw new Exception("Connection failed: " . $conn->connect_error);
+    //         }
 
-                $id = $post_id; // Acessando o 'id' de cada post
+    //             $id = $post_id; // Acessando o 'id' de cada post
             
-                $query = "SELECT * FROM comments
-                WHERE post_id = ?";
+    //             $query = "SELECT * FROM comments
+    //             WHERE post_id = ?";
 
-                $statement = $conn->prepare($query);
+    //             $statement = $conn->prepare($query);
 
-                $statement->bind_param('i', $id);
+    //             $statement->bind_param('i', $id);
 
-                $statement->execute();
+    //             $statement->execute();
 
-                $result = $statement->get_result();
+    //             $result = $statement->get_result();
 
-                $data = $result->fetch_object('Comment');
+    //             $data = $result->fetch_object('Comment');
     
-                $result->free();
+    //             $result->free();
     
-                if(!$data){
-                    throw new Exception("Be the first to Comment!");
-                }
+    //             if(!$data){
+    //                 throw new Exception("Be the first to Comment!");
+    //             }
                 
-                Connection::endConn();
+    //             Connection::endConn();
 
-                return $data;
-            }
-        }
+    //             return $data;
+    //         }
+    //     }

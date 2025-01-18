4b449ffdf3f30fec7c4e36900a448a22e93ba758
gfloriano11 <gflorianodev@gmail.com>
2025-01-17 22:39:27 -0300

    feat: create func to insert post in db

--- class2/src/app/model/Comment.php
+++ class2/src/app/model/Comment.php
@@ -37,6 +37,29 @@
             return $data;
         }
 
+        public static function createPost($comment){
+
+            $conn = Connection::getConn();
+
+            foreach($comment as $key => $value){
+                $$key = $value;
+            }
+
+            $query = "INSERT INTO comments
+            (post_id, user_name, content)
+            VALUES
+            (?, ?, ?)";
+
+            $statement = $conn->prepare($query);
+
+            $statement->bind_param('iss', $post_id, $username, $content);
+
+            $statement->execute();
+
+            Connection::endConn();
+
+        }
+
         public static function deleteCommentsByPost($post_id){
             
             $conn = Connection::getConn();
